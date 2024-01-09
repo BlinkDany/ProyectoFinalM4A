44a9a0c7edd7684d7ac5a34e6fa3aa6eae3c745a
Juanc Idrovo <juancidrovo@gmail.com>
2024-01-09 12:45:50 -0500

    service estudiante
    .....

--- ArteVivoApp/src/main/java/com/examen/demo/models/Service/EstudianteServiceLmpl.java
+++ ArteVivoApp/src/main/java/com/examen/demo/models/Service/EstudianteServiceLmpl.java
@@ -1,5 +1,45 @@
 package com.examen.demo.models.Service;
 
-public class EstudianteServiceLmpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.examen.demo.models.Dao.IEstudianteDao;
+import com.examen.demo.models.Entity.Estudiante;
+
+@Service
+public class EstudianteServiceLmpl implements IEstudianteService{
+	
+	@Autowired
+	IEstudianteDao estudianteDao;
+
+	@Override
+	@Transactional (readOnly = true)
+	public List<Estudiante> findAll() {
+		
+		return (List<Estudiante>) estudianteDao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Estudiante save(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		return estudianteDao.save(estudiante);
+	}
+
+	@Override
+	public Estudiante findById(Long cod_estudiante_pk) {
+		// TODO Auto-generated method stub
+		return estudianteDao.findById(cod_estudiante_pk).orElse(null);
+	}
+
+	@Override
+	public void delete(Long cod_estudiante_pk) {
+		// TODO Auto-generated method stub
+		estudianteDao.deleteById(cod_estudiante_pk);
+	}
 
 }
+
+
