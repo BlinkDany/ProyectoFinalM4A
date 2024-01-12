b65f0abdd40a41b0799154d520a9561055d95baa
Juanc Idrovo <juancidrovo@gmail.com>
2024-01-12 10:49:13 -0500

    Update Matricula.java
    no hice nada ...

--- ArteVivoApp/src/main/java/com/examen/demo/models/Entity/Matricula.java
+++ ArteVivoApp/src/main/java/com/examen/demo/models/Entity/Matricula.java
@@ -2,12 +2,16 @@ package com.examen.demo.models.Entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,7 +32,9 @@ public class Matricula implements Serializable {
 	
 	@Column(nullable = false)
 	private Long cod_estudiante_fk;
+	
 
+	
 	public Long getCod_matricula_pk() {
 		return cod_matricula_pk;
 	}
